fe4a3d5dbdeeb9750660be9ea057d15d518f4497
Andrii <softwaredev210@gmail.com>
2023-04-22 16:27:48 +0900

    test: authcontroller --7

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -71,22 +71,25 @@ class AuthController extends Controller
             return response()->json(['error' => 'Invalid credentials provided.'], 422);
         }
 
+        list($firstname, $lastname) = explode(" ", $googleUser->getName());
         $user = User::firstOrCreate(
                 [
                     'email' => $googleUser->getEmail(),
                 ],
                 [
                     'email_verified_at' => now(),
-                    'name' => $googleUser->getName(),
+                    'first_name' => $firstname,
+                    'last_name' => $lastname,
                     'google_id' => $googleUser->getId(),
                     'avatar' => $googleUser->getAvatar(),
                 ]
             );
-        print_r($user);
 
+        print_r($user->createToken('google-token')->plainTextToken);
+        
         return response()->json([
             'user' => $user,
-            'access_token' => $user->createToken('google-token')->plainTextToken,
+            // 'access_token' => $user->createToken('google-token')->plainTextToken,
             'token_type' => 'Bearer',
         ]);
     }
