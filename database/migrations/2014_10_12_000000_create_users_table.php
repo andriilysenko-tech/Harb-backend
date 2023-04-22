68ca79e96bde631f3f0b1942595bdca1a9fdc2ca
Andrii <softwaredev210@gmail.com>
2023-04-22 18:28:30 +0900

    update: password to nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('phone')->unique();
             $table->string('otp')->nullable();
             $table->string('avatar')->nullable();
